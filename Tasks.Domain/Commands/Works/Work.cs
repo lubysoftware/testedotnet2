d1f205976b894305e1182d37c12b6c807f0a360f
Paulo Ricardo Busch <paulo202015@outlook.com.br>
2020-12-29 20:27:39 -0300

    Entidade Work

---
+++ Tasks.Domain/Commands/Works/Work.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Tasks.Domain.Utils.Bases;
+
+namespace Tasks.Domain.Commands.Works
+{
+    public class Work : EntityBase
+    {
+        public DateTime StartTime { get; private set; }
+        public DateTime EndTime { get; private set; }
+        public string Comment { get; private set; }
+
+        protected Work() : base() { }
+
+        public Wrok(
+            Guid id, 
+            DateTime startTime,
+            DateTime endTime,
+            string comment
+        ) : base(id)
+        {
+            this.SetData(
+                startTime: startTime,
+                endTime: endTime,
+                comment: comment
+            );
+        }
+
+        public void SetData(
+            DateTime startTime,
+            DateTime endTime,
+            string comment
+        ) {
+            StartTime = startTime;
+            EndTime = endTime;
+            Comment = comment;
+        }
+    }
+}
