84e724b58e1a61103627d423b3e91fb515c1db9e
Paulo Ricardo Busch <paulo202015@outlook.com.br>
2020-12-30 19:41:36 -0300

    Interface IAuthService

---
+++ Tasks.Domain/Developers/Dtos/TokenDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Tasks.Domain.Developers.Dtos
+{
+    public class TokenDto
+    {
+        public Guid DeveloperId { get; set; }
+        public string Login { get; set; }
+        public string Token { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime ExpiresAt { get; set; }
+    }
+}
