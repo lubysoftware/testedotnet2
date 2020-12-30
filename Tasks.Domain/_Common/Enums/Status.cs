a58b3f23c8b33e009a39f30f4bc68c6f87740723
Paulo Ricardo Busch <paulo202015@outlook.com.br>
2020-12-30 19:35:11 -0300

    Resultado genérico

---
+++ Tasks.Domain/_Common/Enums/Status.cs
@@ -0,0 +1,11 @@
+﻿namespace Tasks.Domain._Common.Enums
+{
+    public enum Status
+    {
+        Success = 200,
+        Unauthorized = 401,
+        NotFund = 404,
+        Conflict = 409,
+        Invalid = 412
+    }
+}
---
+++ Tasks.Domain/_Common/Results/Result.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+using Tasks.Domain._Common.Enums;
+
+namespace Tasks.Domain._Common.Results
+{
+    public class Result<T> where T : class
+    {
+        [JsonIgnore] public Status Status { get; protected set; }
+        public int? TotalRows { get; protected set; }
+        public T Data { get; protected set; }
+        public bool Success => Status == Status.Success && !_errorMessages.Any();
+        public IEnumerable<string> ErrorMessages => _errorMessages.Any() ? _errorMessages.AsReadOnly() : null;
+
+        protected readonly List<string> _errorMessages;
+
+        public Result()
+        {
+            Status = Status.Success;
+            _errorMessages = new List<string>();
+        }
+
+        public Result(T data, int? total = null) : this()
+        {
+            Data = data;
+            TotalRows = total;
+        }
+
+        public Result(Status status, string error) : this()
+        {
+            _errorMessages.Add(error);
+            Status = status;
+        }
+
+        public Result(Status status, IEnumerable<string> errors) : this()
+        {
+            _errorMessages.AddRange(errors);
+            Status = status;
+        }
+    }
+
+    public class Result : Result<dynamic>
+    {
+        public Result() : base() { }
+
+        public Result(dynamic data) : base()
+        {
+            Data = data;
+        }
+
+        public Result(Status status, string error) : base(status, error) { }
+
+        public Result(Status status, IEnumerable<string> errors) : base(status, errors) { }
+    }
+}
