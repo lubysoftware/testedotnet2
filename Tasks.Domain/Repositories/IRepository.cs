6e962068a1e5eb799db55a45543c73f79c0a01f7
Paulo Ricardo Busch <paulo202015@outlook.com.br>
2020-12-30 19:02:02 -0300

    Interface IRepository

---
+++ Tasks.Domain/Repositories/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Tasks.Domain._Common.Entities;
+
+namespace Tasks.Domain.Repositories
+{
+    public interface IRepository<TEntity> where TEntity : EntityBase
+    {
+        IQueryable<TEntity> Query();
+        Task<bool> ExistsAsync(Guid id);
+        Task<TEntity> GetByIdAsync(Guid id);
+        Task<IEnumerable<TEntity>> GetAllAsync();
+        Task CreateAsync(TEntity entity);
+        Task UpdateAsync(TEntity entity);
+        Task DeleteAsync(TEntity entity);
+    }
+}
