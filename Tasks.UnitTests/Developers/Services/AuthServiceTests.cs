55ba92f326e9a65f6a791f387f7b63f1ef27a87b
Paulo Ricardo Busch <paulo202015@outlook.com.br>
2020-12-31 10:32:10 -0300

    Melhoria na cobertura de teste unitário da AuthService

--- Tasks.UnitTests/Developers/Services/AuthServiceTests.cs
+++ Tasks.UnitTests/Developers/Services/AuthServiceTests.cs
@@ -1,5 +1,6 @@
 ﻿using Moq;
 using System;
+using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using Tasks.Domain._Common.Security;
@@ -7,6 +8,7 @@ using Tasks.Domain.Developers.Dtos;
 using Tasks.Domain.Developers.Repositories;
 using Tasks.Domain.Developers.Services;
 using Tasks.UnitTests._Common;
+using Tasks.UnitTests._Common.Random;
 using Xunit;
 
 namespace Tasks.UnitTests.Developers.Services
@@ -25,45 +27,49 @@ namespace Tasks.UnitTests.Developers.Services
             };
         }
 
-        [Fact]
-        public async void LoginWithSuccessTest()
+        public static IEnumerable<object[]> LoginDeveloperData()
         {
-            var password = "senha";
-            var developer = EntitiesFactory.NewDeveloper(password: password).Get();
-            var loginDto = new LoginDto { Login = developer.Login, Password = password };
-            _developerRepository.Setup(d => d.ExistByLoginAsync(developer.Login)).ReturnsAsync(true);
-            _developerRepository.Setup(d => d.FindByLoginAsync(developer.Login)).ReturnsAsync(developer);
+            var loginDto = new LoginDto { Login = RandomHelper.RandomString(), Password = RandomHelper.RandomNumbers() };
+            yield return new object[] { false, loginDto };
+            yield return new object[] { false, loginDto, true };
+            yield return new object[] { true,  loginDto, true, true };
+        }
 
+        [Theory]
+        [MemberData(nameof(LoginDeveloperData))]
+        public async void LoginDeveloperTest(
+            bool expectedSuccess,
+            LoginDto loginDto,
+            bool persistedDeveloper = false,
+            bool equalPassword = false
+        ) {
+            var developer = EntitiesFactory.NewDeveloper(
+                login: loginDto.Login,
+                password: equalPassword ? loginDto.Password : null
+            ).Get();
+            if (persistedDeveloper)
+            {
+                _developerRepository.Setup(d => d.ExistByLoginAsync(developer.Login)).ReturnsAsync(true);
+                _developerRepository.Setup(d => d.FindByLoginAsync(developer.Login)).ReturnsAsync(developer);
+            }
+            
             var service = new AuthService(_developerRepository.Object, _tokenConfiguration);
             var result = await service.LoginAsync(loginDto);
 
             var data = result.Data;
-            Assert.True(result.Success);
-            Assert.Equal(developer.Id, data.Id);
-            Assert.Equal(developer.Login, data.Login);
-            Assert.Equal(TimeSpan.FromSeconds(_tokenConfiguration.Seconds), data.ExpiresAt - data.CreatedAt);
-            Assert.NotEmpty(data.Token);
-            Assert.StartsWith("Bearer", data.Token);
-            var jwt = new JwtSecurityToken(data.Token.Split("Bearer").Last());
-            Assert.NotEmpty(jwt.Claims);
-        }
-
-        [Theory]
-        [InlineData("")]
-        [InlineData("asdfd")]
-        [InlineData("32156")]
-        public async void LoginWithRejectTest(string password)
-        {
-            var developer = EntitiesFactory.NewDeveloper(password: "senha").Get();
-            var loginDto = new LoginDto { Login = developer.Login, Password = password };
-            _developerRepository.Setup(d => d.ExistByLoginAsync(developer.Login)).ReturnsAsync(true);
-            _developerRepository.Setup(d => d.FindByLoginAsync(developer.Login)).ReturnsAsync(developer);
-
-            var service = new AuthService(_developerRepository.Object, _tokenConfiguration);
-            var result = await service.LoginAsync(loginDto);
-
-            Assert.False(result.Success);
-            Assert.Null(result.Data);
+            Assert.Equal(expectedSuccess, result.Success);
+            if (expectedSuccess) { 
+                Assert.Equal(developer.Id, data.Id);
+                Assert.Equal(developer.Login, data.Login);
+                Assert.Equal(TimeSpan.FromSeconds(_tokenConfiguration.Seconds), data.ExpiresAt - data.CreatedAt);
+                Assert.NotEmpty(data.Token);
+                Assert.StartsWith("Bearer", data.Token);
+                var jwt = new JwtSecurityToken(data.Token.Split("Bearer").Last());
+                Assert.NotEmpty(jwt.Claims);
+            } else
+            {
+                Assert.Null(data?.Token);
+            }
         }
     }
 }
