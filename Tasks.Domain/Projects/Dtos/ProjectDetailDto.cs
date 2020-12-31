4b063d8411c9f3e86237ae5d59f968649af48ff3
Paulo Ricardo Busch <paulo202015@outlook.com.br>
2020-12-31 15:03:42 -0300

    Dtos do Project

---
+++ Tasks.Domain/Projects/Dtos/ProjectDetailDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Tasks.Domain.Projects.Dtos
+{
+    public class ProjectDetailDto
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
