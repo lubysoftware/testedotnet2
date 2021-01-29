f7e4a6350b56718176c7e3f3c5e3a2e7386cc9db
danilo <dsf.eng17@uea.edu.br>
2021-01-29 19:57:02 -0400

    Migrations e Sql Server
    Configuração dos dois itens concluída

--- API_LancamentoHoras/API_LancamentoHoras/Startup.cs
+++ API_LancamentoHoras/API_LancamentoHoras/Startup.cs
@@ -28,7 +28,8 @@ namespace API_LancamentoHoras
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<Contexto>(opt => opt.UseInMemoryDatabase("Lista"));
+            //services.AddDbContext<Contexto>(opt => opt.UseInMemoryDatabase("Lista"));
+            services.AddDbContext<Contexto>(options => options.UseSqlServer(Configuration.GetConnectionString("StringDeConexao")));
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
